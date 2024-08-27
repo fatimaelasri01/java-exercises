54b3e591a03c8cc4e40c8c478865eb5d79422569
Malikaelabderrahmani <malikarahmani67@gmail.com>
2024-08-27 16:06:45 +0100

    sdfghj

---
+++ malika_solutions/addition.java
@@ -0,0 +1,10 @@
+package malika_solutions;
+
+public class addition {
+    double nb1;
+    double nb2;
+    public static double addit(double nb1, double nb2)
+    {
+        return (nb1 + nb2);
+    }
+}
